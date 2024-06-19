6219ca05a0fd1c60627d3bc4e93fca324cae8412
HoangThanhLong <long17188@gmail.com>
2024-06-19 21:39:38 +0700

    [UPDATE] Xóa một sản phẩm (admin)

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -76,6 +76,7 @@ module.exports.changeMulti = async (req,res) => {
 module.exports.deleteItem = async (req,res) => {
     const id = req.params.id;
 
-    await Product.deleteOne({ _id: id});
+    // await Product.deleteOne({ _id: id});
+    await Product.updateOne({ _id: id}, { deleted: true, deleteAt: new Date()});
     res.redirect("back");
 };
