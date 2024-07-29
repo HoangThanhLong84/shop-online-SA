83b2d8c9d9a9d53727e59d0e67013a11decae5eb
HoangThanhLong <long17188@gmail.com>
2024-07-29 23:43:58 +0700

    fix middleware

--- middlewares/client/cart.middleware.js
+++ middlewares/client/cart.middleware.js
@@ -5,8 +5,8 @@ module.exports.cartId = async (req, res, next) => {
     const cart = await Cart.findOne({
         _id: req.cookies.cartId
     });
-    cart.totalQuantity = cart.products.reduce((sum, item) => sum + item.quantity,0);
+    if(req.cookies.cartId){cart.totalQuantity = cart.products.reduce((sum, item) => sum + item.quantity,0);};
+    
     res.locals.miniCart = cart;
     next();
-     
 }
